4f39c19eb34a91a2c661aab45bd6c5cc9d195e6a
Julian Rooze <julian@newblack.io>
2025-11-22 02:02:59 +0100

    .

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$ROOT_DIR"
 
-dotnet restore Assertive.sln
-dotnet build Assertive.sln -c Release --no-restore
-dotnet test Assertive.sln -c Release --no-build
+dotnet restore src/Assertive.sln
+dotnet build src/Assertive.sln -c Release --no-restore
+dotnet test src/Assertive.sln -c Release --no-build
